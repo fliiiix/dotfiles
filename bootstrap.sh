fe9f77e89a6e976456dacadfca5a521311cb121d
fliiiix <de-ch@hotmail.de>
2015-09-06 17:14:22 +0200

    fix ssh keysetup

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,15 @@
 sudo dnf install -y ansible
 ssh-keygen -b 4096 -f $HOME/.ssh/ansible -N ''
 cat $HOME/.ssh/ansible.pub >> $HOME/.ssh/authorized_keys
+
+touch $HOME/.ssh/config
+cat <<EOT >> $HOME/.ssh/config
+Host 127.0.0.1
+    IdentityFile ~/.ssh/ansible
+EOT
+
 sudo chmod 600 $HOME/.ssh/*
 sudo systemctl enable sshd
 sudo service sshd start
 
-sudo sh -c 'echo "127.0.0.1" >> /etc/ansible/hosts'
+sudo sh -c 'echo "127.0.0.1" > /etc/ansible/hosts'
