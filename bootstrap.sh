c0e2e3169336e4b8b66776cf23b7513e573e6b8f
fliiiix <de-ch@hotmail.de>
2015-09-06 17:00:59 +0200

    dnf auto install, key without password

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # setup and start ssh
-sudo dnf install ansible
-ssh-keygen -b 4096 -f $HOME/.ssh/ansible
+sudo dnf install -y ansible
+ssh-keygen -b 4096 -f $HOME/.ssh/ansible -N ''
 cat $HOME/.ssh/ansible.pub >> $HOME/.ssh/authorized_keys
 sudo chmod 600 $HOME/.ssh/*
 sudo systemctl enable sshd
 sudo service sshd start
 
-echo "127.0.0.1" >> /etc/ansible/hosts
+sudo sh -c 'echo "127.0.0.1" >> /etc/ansible/hosts'
