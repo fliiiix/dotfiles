7afa2a4f01e2904bdc8cb216411dcda7d6d84bbf
fliiiix <de-ch@hotmail.de>
2016-12-08 20:07:42 +0100

    cleanup and add steam + more structure

--- setup.sh
+++ setup.sh
@@ -2,30 +2,7 @@
 
 #awwwwwwwwwwwwwwww stop using me
 echo "aaaaaaaaaaaa please don't"
-WORK_DIR=$(pwd)
-
-#setup icons
-
-sudo yum install librsvg2-tools
-
-if [ ! -d ~/.icons ]
-   then
-     mkdir ~/.icons
-fi
-
-cd ~/.icons/
-
-if [ ! -d ~/.icons/Clarity ]
-    then
-      git clone https://github.com/jcubic/Clarity.git
-fi
-
-cd ~/.icons/Clarity/
-git pull
-./configure
-make
-
-cd $WORK_DIR
+exit 1
 
 #copy themes 
 if [ ! -d ~/.themes ]
@@ -47,12 +24,6 @@ sudo git clone -n https://github.com/pfn/keepasshttp.git --depth 1 /usr/lib/keep
 
 cd /usr/lib/keepass/plugins/keepasshttp
 sudo git checkout HEAD KeePassHttp.plgx
-
-#install steam from rpmfusion
-sudo yum install -y steam
-
-#install vlc from rpmfusion
-sudo yum install -y vlc
 
 #install modules for logitech g510
 sudo curl -o /etc/yum.repos.d/gnome15.repo http://packages.russo79.com/fedora/20/gnome15/gnome15.repo
