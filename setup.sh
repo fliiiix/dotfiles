f75e8530d63d627c7f954d5bf6abd270bb292425
fliiiix <de-ch@hotmail.de>
2016-12-11 12:02:21 +0100

    installing themes

--- setup.sh
+++ setup.sh
@@ -4,14 +4,6 @@
 echo "aaaaaaaaaaaa please don't"
 exit 1
 
-#copy themes 
-if [ ! -d ~/.themes ]
-   then
-     mkdir ~/.themes
-fi
-
-cp -rf ./.themes ~/.themes
-
 #setup keepass don't forget to install the chrome or firefox plugin
 sudo yum install -y keepass
 
@@ -24,7 +16,3 @@ sudo git clone -n https://github.com/pfn/keepasshttp.git --depth 1 /usr/lib/keep
 
 cd /usr/lib/keepass/plugins/keepasshttp
 sudo git checkout HEAD KeePassHttp.plgx
-
-#install modules for logitech g510
-sudo curl -o /etc/yum.repos.d/gnome15.repo http://packages.russo79.com/fedora/20/gnome15/gnome15.repo
-sudo yum install gnome15-suite-other 
