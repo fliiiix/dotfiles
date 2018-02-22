efc58b067ba8848ef33a7fccc9cb6206e7f495ce
fliiiix <de-ch@hotmail.de>
2018-02-22 21:47:24 +0100

    use ansible-playbook -i localhost fix #3 again

--- bootstrap_laptop.sh
+++ bootstrap_laptop.sh
@@ -2,4 +2,4 @@
 
 # setup and start ssh
 sudo dnf install -y ansible python2-dnf
-ansible-playbook -i localhost, laptop.yaml --ask-become-pass
+ansible-playbook -i localhost, -c local laptop.yaml --ask-become-pass
