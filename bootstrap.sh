103e239c115fcb286edc175f1d10a8044d078a08
fliiiix <de-ch@hotmail.de>
2018-05-12 11:43:18 +0200

    always install libselinux-python for file permisions

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 case $1 in
    laptop|desktop)
      echo "I'm setting up your $1"
-     sudo dnf install -y ansible python2-dnf
+     sudo dnf install -y ansible python2-dnf libselinux-python
      ansible-playbook -i localhost, -c local --ask-become-pass setups/$1.yml ;;
    *)
      echo "Please use $0 laptop|desktop" ;;
