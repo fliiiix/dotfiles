bb317a6ccf8cd51cd8d175f60676fa93ec046ec4
fliiiix <hi@l33t.name>
2019-12-09 22:02:44 +0100

    Drop ansible_python_interpreter since Fedora 31 defaults to python3

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ case $1 in
    laptop|desktop)
      echo "I'm setting up your $1"
      sudo dnf install -y ansible python3-dnf
-     ansible-playbook --extra-vars "ansible_python_interpreter=/usr/bin/python3" -i localhost, -c local --ask-become-pass setups/$1.yml ;;
+     ansible-playbook -i localhost, -c local --ask-become-pass setups/$1.yml ;;
    *)
      echo "Please use $0 laptop|desktop" ;;
 esac
