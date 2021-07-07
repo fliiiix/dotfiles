4737565e5fbc4583cf40a01fb307df8fea0ed628
fliiiix <hi@l33t.name>
2021-07-07 20:57:12 +0200

    update how ssh keys are generated

--- config/zsh/lib/sshcreate.zsh
+++ config/zsh/lib/sshcreate.zsh
@@ -4,5 +4,6 @@ sshcreate() {
         echo "sshcreate <keyname>"
         return 1
     fi
-    ssh-keygen -b 4096 -f $1 -C $MAIL && echo "your public key:" && cat $1.pub
+    # https://security.stackexchange.com/a/144044
+    ssh-keygen -t ed25519 -a 100 -f $1 -C $MAIL && echo "your public key:" && cat $1.pub
 }
