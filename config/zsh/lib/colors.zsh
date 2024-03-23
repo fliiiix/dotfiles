85e1e9b9cc92b580181b021ac4f4593ccc0bd789
fliiiix <hi@l33t.name>
2024-03-23 18:01:28 +0100

    ZSH use default LS_COLORS to work better with light themes

--- config/zsh/lib/colors.zsh
+++ config/zsh/lib/colors.zsh
@@ -1,7 +1,9 @@
 typeset -Ag FX FG BG
 
-LSCOLORS="exfxcxdxbxegedabagacad"
-LS_COLORS="di=34;40:ln=35;40:so=32;40:pi=33;40:ex=31;40:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:"
+# use default LS_COLOR
+# https://askubuntu.com/questions/466198/how-do-i-change-the-color-for-directories-with-ls-in-the-console/466203#466203
+#LSCOLORS="exfxcxdxbxegedabagacad"
+#LS_COLORS="di=34;40:ln=35;40:so=32;40:pi=33;40:ex=31;40:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:"
 
 FX=(
     reset     "%{[00m%}"
